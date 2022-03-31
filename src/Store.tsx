6aae437f6d285a005bc7018d7fef51147607a67b
Heytemisan <temyicy@gmail.com>
2022-03-31 06:55:17 +0100

    we 'data' from the store with the intiateState ~ episodes & favorite

--- src/Store.tsx
+++ src/Store.tsx
@@ -10,17 +10,18 @@ const initialState: IState = { //populate initialState
     favourites: [],
 }
 
-//create the store & then pass "episodes & favourite" from initialState
-export const Store = React.createContext<IState | any>(initialState); //[pass]
-
 export interface IAction {
     type: string,
     payload: any
 }
 
+//create the store & then pass "episodes & favourite" from initialState
+
+export const Store = React.createContext<IState | any>(initialState); //front-end store [pass]
+
 //Reducer manipulates the store
 function reducer(state: IState, action: IAction): IState {//[pass IState as a generic]
-    switch (action.type) {
+    switch (action.type) {//IAction ~ type string, which fetches the data from the api.
         case 'FETCH_DATA': //checked
             return { ...state, episodes: action.payload }
         case 'ADD_FAV':
@@ -32,6 +33,11 @@ function reducer(state: IState, action: IAction): IState {//[pass IState as a ge
 
 export function StoreProvider(props: any): JSX.Element {
 //StoreProvider gives the component in the app access to the store.
+//value: state & dispatch is passed to App.tsx;
+//dispatch(argument) triggers the action
+//state is the value.
+//useReducer has a default value of reducer & and initialState
+    
     const [state, dispatch] = React.useReducer(reducer, initialState)
     return (
         <Store.Provider value={{state, dispatch}}>
