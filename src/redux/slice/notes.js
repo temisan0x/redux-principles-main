524d0df2054e12a51042dda6b259b6b010f46441
Heytemisan <temyicy@gmail.com>
2022-06-05 12:10:47 +0100

    chore: restructured slice initialState

--- src/redux/slice/notes.js
+++ src/redux/slice/notes.js
@@ -4,29 +4,33 @@ let lastId = 0;
 
 export const slice = createSlice({
     name: "notes",
-    initialState: [],
+    initialState: {
+        loading: false,
+        list: [],
+        fetchedList: null,
+    },
     reducers: {
         noteAssignedToUser: (notes, action) => {
             const { noteId, userId } = action.payload;
-            const index = notes.findIndex(note => note.id === noteId);
-            notes[index].userId = userId;
+            const index = notes.list.findIndex(note => note.id === noteId);
+            notes.list[index].userId = userId;
         },
         noteAdded: (notes, action) => {
-            notes.push({
+            notes.list.push({
                 id: ++lastId,
                 description: action.payload.description,
                 resolved: false
             })
         },
         noteResolved: (notes, action) => {
-            const index = notes.findIndex(note => note.id === action.payload.id);
-            notes[index].resolved = true;
+            const index = notes.list.findIndex(note => note.id === action.payload.id);
+            notes.list[index].resolved = true;
         }
     }
 })
 
 //selector is a function that takes a state and returns the computed state.
-export const getUnresolvedNotes = state =>  state.entities.notes.filter(note => !note.resolved);
+export const getUnresolvedNotes = state => state.entities.notes.filter(note => !note.resolved);
 export const getNotesByUser = state => state.entities.notes.filter(note => note.userId === !note.userId);
 
 export const { noteAdded, noteResolved, noteAssignedToUser } = slice.actions;
